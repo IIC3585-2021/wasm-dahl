70c58da55b5ea4a12b6e6c22513365a4bccad2d5
beayancan <beayancan@uc.cl>
2021-05-01 21:44:30 -0400

    debuging c

--- dijkstra-utils.js
+++ dijkstra-utils.js
@@ -10,82 +10,18 @@ Module().then(function(mymod) {
   }
 )
 
-const getGraph = (n=7) => {
-  let Graph = [];
-  for (let i = 0; i < n; i++) {
-    Graph[i] = new Array(n);
-  }
-
-  Graph[0][0] = 0;
-  Graph[0][1] = 0;
-  Graph[0][2] = 1;
-  Graph[0][3] = 2;
-  Graph[0][4] = 0;
-  Graph[0][5] = 0;
-  Graph[0][6] = 0;
-
-  Graph[1][0] = 0;
-  Graph[1][1] = 0;
-  Graph[1][2] = 2;
-  Graph[1][3] = 0;
-  Graph[1][4] = 0;
-  Graph[1][5] = 3;
-  Graph[1][6] = 0;
-
-  Graph[2][0] = 1;
-  Graph[2][1] = 2;
-  Graph[2][2] = 0;
-  Graph[2][3] = 1;
-  Graph[2][4] = 3;
-  Graph[2][5] = 0;
-  Graph[2][6] = 0;
-
-  Graph[3][0] = 2;
-  Graph[3][1] = 0;
-  Graph[3][2] = 1;
-  Graph[3][3] = 0;
-  Graph[3][4] = 0;
-  Graph[3][5] = 0;
-  Graph[3][6] = 1;
-
-  Graph[4][0] = 0;
-  Graph[4][1] = 0;
-  Graph[4][2] = 3;
-  Graph[4][3] = 0;
-  Graph[4][4] = 0;
-  Graph[4][5] = 2;
-  Graph[4][6] = 0;
-
-  Graph[5][0] = 0;
-  Graph[5][1] = 3;
-  Graph[5][2] = 0;
-  Graph[5][3] = 0;
-  Graph[5][4] = 2;
-  Graph[5][5] = 0;
-  Graph[5][6] = 1;
-
-  Graph[6][0] = 0;
-  Graph[6][1] = 0;
-  Graph[6][2] = 0;
-  Graph[6][3] = 1;
-  Graph[6][4] = 0;
-  Graph[6][5] = 1;
-  Graph[6][6] = 0;
-
-  return Graph;
-}
-
 const cRunner = (Module) => {
-  let graph = getGraph();
+  //let graph = getGraph();
+		
+
   let startTime = window.performance.now();
-  let result = Module._dijkstra(graph, 7, 0);
+  let result = Module._dijkstra(7, 0);
   let endTime = window.performance.now();
 
   console.log('result', result);
   return result, endTime - startTime
 }
 
-// aqui arroja error
 const addToTable = (resultArr) => {
   const table = document.getElementById("result-table");
   let row = table.insertRow(-1);
--- dijkstra.c
+++ dijkstra.c
@@ -1,50 +1,121 @@
-// Dijkstra's Algorithm in C
-
 #include <stdio.h>
+#include <stdlib.h>
+
 #define INFINITY 9999
-#define MAX 10
-
-int dijkstra(int Graph[MAX][MAX], int n, int start) {
+#define MAX 5
   
-  int cost[MAX][MAX], distance[MAX], pred[MAX];
-  int visited[MAX], count, mindistance, nextnode, i, j;
+void main()
+{
+  int n = 5;
+  int startnode = 0;
+ 
+	int cost[MAX][MAX],distance[MAX],pred[MAX];
+	int visited[MAX],count,mindistance,nextnode,i,j;
+	
+	
+  long long G[5][5];
+	
+	//pred[] stores the predecessor of each node
+	//count gives the number of nodes seen so far
+	//create the cost matrix
+	G[0][0] = 0;
+  G[0][1] = 10;
+  G[0][2] = 0;
+  G[0][3] = 30;
+  G[0][4] = 100;
 
-  // Creating cost matrix
-  for (i = 0; i < n; i++)
-    for (j = 0; j < n; j++)
-      if (Graph[i][j] == 0)
-        cost[i][j] = INFINITY;
-      else
-        cost[i][j] = Graph[i][j];
+  G[1][0] = 10;
+  G[1][1] = 0;
+  G[1][2] = 50;
+  G[1][3] = 0;
+  G[1][4] = 0;
 
-  for (i = 0; i < n; i++) {
-    distance[i] = cost[start][i];
-    pred[i] = start;
-    visited[i] = 0;
-  }
+  G[2][0] = 0;
+  G[2][1] = 0;
+  G[2][2] = 0;
+  G[2][3] = 20;
+  G[2][4] = 10;
 
-  distance[start] = 0;
-  visited[start] = 1;
-  count = 1;
+  G[3][0] = 30;
+  G[3][1] = 0;
+  G[3][2] = 20;
+  G[3][3] = 0;
+  G[3][4] = 60;
 
-  while (count < n - 1) {
-    mindistance = INFINITY;
+  G[4][0] = 100;
+  G[4][1] = 0;
+  G[4][2] = 10;
+  G[4][3] = 60;
+  G[4][4] = 0;
+  
 
-    for (i = 0; i < n; i++)
-      if (distance[i] < mindistance && !visited[i]) {
-        mindistance = distance[i];
-        nextnode = i;
-      }
+	for (i = 0; i < n; i++)
+		for( j = 0; j < n; j++)
 
-    visited[nextnode] = 1;
-    for (i = 0; i < n; i++)
-      if (!visited[i])
-        if (mindistance + cost[nextnode][i] < distance[i]) {
-          distance[i] = mindistance + cost[nextnode][i];
-          pred[i] = nextnode;
-        }
-    count++;
-  }
+      printf(" i: %d, j: %d  ---> %lld\n", i, j, G[i][j]);
 
-  return 999;
+			if( G[i][j] == 0 )
+				cost[i][j]=INFINITY;
+			else
+				cost[i][j]=G[i][j];
+	
+	//initialize pred[],distance[] and visited[]
+	for(i=0;i<n;i++)
+	{
+		distance[i]=cost[startnode][i];
+		pred[i]=startnode;
+		visited[i]=0;
+	}
+	
+	distance[startnode]=0;
+	visited[startnode]=1;
+	count=1;
+	
+	while(count<n-1)
+	{
+		mindistance=INFINITY;
+		
+		//nextnode gives the node at minimum distance
+		for(i=0;i<n;i++)
+			if(distance[i]<mindistance&&!visited[i])
+			{
+				mindistance=distance[i];
+				nextnode=i;
+			}
+			
+			//check if a better path exists through nextnode			
+			visited[nextnode]=1;
+			for(i=0;i<n;i++)
+				if(!visited[i])
+					if(mindistance+cost[nextnode][i]<distance[i])
+					{
+						distance[i]=mindistance+cost[nextnode][i];
+						pred[i]=nextnode;
+					}
+		count++;
+	}
+
+  //Process* process = calloc(1, sizeof(Process));
+  //int* arreglo = calloc(100, sizeof(int));
+ 
+	//print the path and distance of each node
+	for(i = 0; i < n; i++)
+		if ( i != startnode)
+		{
+			printf("\nDistance of node%d=%d",i,distance[i]);
+			printf("\nPath=%d",i);
+			
+			j = i;
+			do
+			{
+				j = pred[j];
+				printf("<-%d",j);
+			} while( j!= startnode);
+
+      //arreglo[i] = distance[i];
+	}
+
+
+  //return distance;
+
 }
